cedb31ac9eff3c3bfb94e2f66394cda09a50f7ae
Ridwan2638 <sanniridwanolamide@gmail.com>
2026-07-25 15:05:30 +0100

    Preloader css for mobile and desktop updated

--- src/preloader/Preloader.jsx
+++ src/preloader/Preloader.jsx
@@ -3,11 +3,17 @@ import residebase from "../assets/residebase.gif";
 
 const Preloader = () => {
   return (
-    <div className=" flex flex-col w-full h-full items-center">
-      <img src={residebase} className="w-[30rem] mt-10" alt="Reside Project Ongoing" />
-      <p className="font-inter font-semibold text-3xl mt-15">
-        Watch Out, Our developer are working on the Project
-      </p>
+    <div className="relative h-screen w-screen justify-center flex items-center">
+      <div className=" relative flex flex-col items-center mx-4">
+        <img
+          src={residebase}
+          className="w-2xs h-3xs desktop:w-lg desktop:h-md"
+          alt="Reside Project Ongoing"
+        />
+        <p className="font-inter font-semibold text-2xl mt-10 text-center">
+          Watch Out, Our developer are working on the Project
+        </p>
+      </div>
     </div>
   );
 };
